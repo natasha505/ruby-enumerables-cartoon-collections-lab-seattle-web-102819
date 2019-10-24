56d02ea3afc57428c8ff5d5c9538ac44f85c44a2
Natali kruk <natasha_505@yahoo.com>
2019-10-24 02:57:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,15 +5,18 @@ def roll_call_dwarves(names)
 end
 
 
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |calls| calls.capitalize + "!" }
-  
 end
+
+
 
 def long_planeteer_calls(long_calls)
   long_calls.any? { |call| call.length > 4}
-  
 end
+
+
 
 def find_the_cheese(food_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
